38e31c6530de89b5018ad152042c902c888a2922
ivantellop <tellezlopezcesar@gmail.com>
2025-02-23 17:27:28 -0600

    Selecciono los elementos del DOM con los que voy a trabajar

---
+++ script.js
@@ -0,0 +1,7 @@
+// Espera a que todo el contenido de la página cargue antes de ejecutar el script
+document.addEventListener("DOMContentLoaded", () => {
+  // Seleccionamos los elementos del DOM con los que vamos a interactuar
+  const commentInput = document.getElementById("commentInput"); // Caja de comentarios
+  const addCommentBtn = document.getElementById("addCommentBtn"); // Botón para agregar el comentario
+  const commentList = document.getElementById("commentList"); // Lista donde se mostrarán los comentarios
+});
